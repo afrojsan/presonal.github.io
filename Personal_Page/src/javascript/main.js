0dd1ffb45c27d694d1a90d19969d81529cad6389
afrojsan <chung86813@gmail.com>
2021-09-21 18:29:32 +0800

    fix navbar

---
+++ Personal_Page/src/javascript/main.js
@@ -0,0 +1,18 @@
+const navBar = document.querySelector('#navBar')
+const navBarLogo  = document.querySelector('#navBarLogo')
+const wholeNarBar = document.querySelector('#wholeNarBar')
+
+function show() {
+    navBar.style.display = 'none'
+    wholeNarBar.style.transition = 'width 2000ms'
+    wholeNarBar.style.display = 'flex'
+}
+
+navBar.addEventListener('click', show)
+
+function noneShow() {
+    wholeNarBar.style.display = 'none'
+    navBar.style.display = 'flex'
+}
+
+navBarLogo.addEventListener('click', noneShow)
