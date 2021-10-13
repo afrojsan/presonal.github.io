8a89e8552e10ae0b3a9fc7f3b7ebf38a742ac15e
afrojsan <chung86813@gmail.com>
2021-10-13 22:02:31 +0800

    update ui

---
+++ Personal_Page/src/javascript/main2.js
@@ -0,0 +1,44 @@
+/* --------------------------------------------------------------------- */
+/* ----------------------------Nav-Bar animation------------------------ */
+/* --------------------------------------------------------------------- */
+
+const navBtn = document.querySelector('.navBtn');
+const navLinks = document.querySelector('.navLinks')
+
+function navigation(){
+    navLinks.classList.toggle('open');
+    navLinks.style.transition = 'transform 0.5s ease-in';
+};
+
+navBtn.addEventListener('click', navigation);
+
+/* --------------------------------------------------------------------- */
+/* ----------------------------Stop animation--------------------------- */
+/* --------------------------------------------------------------------- */
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
+
+
+/* --------------------------------------------------------------------- */
+/* ----------------------------scroll top------------------------------- */
+/* --------------------------------------------------------------------- */
+const scrollBtn = document.querySelector('#routeToTop')
+
+function showScrollBtn(){
+    if(window.pageYOffset > 100){
+        scrollBtn.classList.add('show')
+    }else{
+        scrollBtn.classList.remove('show')
+    }
+};
+
+window.addEventListener('scroll', showScrollBtn);
+
+showScrollBtn();
